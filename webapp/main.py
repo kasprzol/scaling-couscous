9a6e00afc456b21b29da197d5fcd9d55b247c36d
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-05-08 16:21:45 +0200

    Implementation of ping endpoint.
    Use FastAPI's status instead of http.HTTPStatus.

--- webapp/main.py
+++ webapp/main.py
@@ -1,22 +1,32 @@
-from http import HTTPStatus
-
-from fastapi import FastAPI
+import aiohttp
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 from .models import InfoResponse, PingError, PingRequest, PingResponse
 
 app = FastAPI()
 
 
-@app.get("/info", response_model=InfoResponse, status_code=HTTPStatus.OK)
+@app.get("/info", response_model=InfoResponse, status_code=status.HTTP_200_OK)
 def info():
-    return {"Receiver": "Cisco is the best!"}
+    return InfoResponse(Receiver="Cisco is the best!")
 
 
 @app.post(
     "/ping",
     response_model=PingResponse,
-    status_code=HTTPStatus.OK,
-    responses={HTTPStatus.BAD_REQUEST: {"model": PingError}},
+    status_code=status.HTTP_200_OK,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": PingError}},
 )
 async def ping(request: PingRequest):
-    pass
+    print(f"downloading {request.url}")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(request.url, ssl=False) as response:
+                payload = await response.text()
+                return PingResponse(response=payload)
+    except Exception as e:
+        error_msg = f"Error while retrieving the requested url: {e}"
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST, content={"error": error_msg}
+        )
