11804682d576952fb1258a74e4ca23cce44f52b5
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-05-08 19:23:20 +0200

    Use proper corn.

--- docker/webapp_entrypoint.sh
+++ docker/webapp_entrypoint.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 # In production this would be at least a number of uwsgi/gunicorn processes
 # behind a proxy like nginx or haproxy.
-poetry run gunicorn -b 0.0.0.0:8000 -w 4 django_project.wsgi:application
+poetry run uvicorn --host 0.0.0.0 webapp.main:app
